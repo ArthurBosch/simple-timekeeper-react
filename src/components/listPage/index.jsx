503b78b9adf3e448f5872f548f01e905d021914b
ArthurBosch <mr.bolshakov12@gmail.com>
2022-09-04 12:40:54 +0300

    list commit

--- src/components/listPage/index.jsx
+++ src/components/listPage/index.jsx
@@ -2,12 +2,23 @@ import { useEffect, useContext } from "react";
 import { AppContext } from "../../App";
 import "./listPage.css";
 import plus from "../../svg/plus.svg";
+import ShiftListItem from "./shiftListItem/shiftListItem";
+import db from "../../db.json";
 
 const ListPage = () => {
   const { changePageName } = useContext(AppContext);
   useEffect(() => {
     changePageName("My Shifts");
   });
+
+  const shifts = db.shifts;
+
+  const renderShifts = () => {
+    return shifts.map((shift) => {
+      return <ShiftListItem props={shift} key={shift.id} />;
+    });
+  };
+
   return (
     <div className="list-page--container" id="list-container">
       <div className="add-shift">
@@ -16,72 +27,7 @@ const ListPage = () => {
           <img src={plus} className="plus-sign" />
         </button>
       </div>
-      <div className="shift-list">
-        <div className="shift-list--item">
-          <div className="shift-list--date">
-            <h4 className="date-number">13</h4>
-            <span className="date-day">sat</span>
-          </div>
-          <div className="shift-list--time">
-            <span className="time-start">12:15</span> -
-            <span className="time-end">21:30</span>
-          </div>
-          <div className="shift-list--info">
-            <button className="info-button">i</button>
-          </div>
-        </div>
-        <div className="shift-list--item">
-          <div className="shift-list--date">
-            <h4 className="date-number">12</h4>
-            <span className="date-day">fri</span>
-          </div>
-          <div className="shift-list--time">
-            <span className="time-start">12:15</span> -
-            <span className="time-end">21:30</span>
-          </div>
-          <div className="shift-list--info">
-            <button className="info-button">i</button>
-          </div>
-        </div>
-        <div className="shift-list--item">
-          <div className="shift-list--date">
-            <h4 className="date-number">11</h4>
-            <span className="date-day">thurs</span>
-          </div>
-          <div className="shift-list--time">
-            <span className="time-start">12:15</span> -
-            <span className="time-end">21:30</span>
-          </div>
-          <div className="shift-list--info">
-            <button className="info-button">i</button>
-          </div>
-        </div>
-        <div className="shift-list--item">
-          <div className="shift-list--date">
-            <h4 className="date-number">10</h4>
-            <span className="date-day">wed</span>
-          </div>
-          <div className="shift-list--time">
-            <span className="time-start">12:15</span> -
-            <span className="time-end">21:30</span>
-          </div>
-          <div className="shift-list--info">
-            <button className="info-button">i</button>
-          </div>
-        </div>
-        <div className="shift-list--item">
-          <div className="shift-list--date">
-            <h4 className="date-number">9</h4>
-            <span className="date-day">tues</span>
-          </div>
-          <div className="shift-list--time">
-            <span className="OFF">OFF</span>
-          </div>
-          <div className="shift-list--info">
-            <button className="info-button">i</button>
-          </div>
-        </div>
-      </div>
+      <div className="shift-list">{renderShifts()}</div>
     </div>
   );
 };
---
+++ src/components/listPage/shiftListItem/shiftListItem.jsx
@@ -0,0 +1,23 @@
+const ShiftListItem = ({ props }) => {
+  const { num, day, shiftStart, shiftEnd } = props;
+  console.log(props);
+
+  return (
+    <div className="shift-list--item">
+      <div className="shift-list--date">
+        <h4 className="date-number">{num}</h4>
+        <span className="date-day">{day}</span>
+      </div>
+      <div className="shift-list--time">
+        <span className="time-start">{shiftStart}</span>
+        <span> - </span>
+        <span className="time-end">{shiftEnd}</span>
+      </div>
+      <div className="shift-list--info">
+        <button className="info-button">i</button>
+      </div>
+    </div>
+  );
+};
+
+export default ShiftListItem;
